4cdaaa99a5bc2ecc6a5b62f5d488cc6a9f57624d
s7k-windows <benotimprotant@mail.ru>
2025-05-22 22:48:09 +0300

    Init

---
+++ draw.py
@@ -0,0 +1,154 @@
+import pygame
+import game as GM
+from game import Game
+
+pygame.init()
+
+WIN_X = 1080
+WIN_Y = 720
+FPS = 30
+clock = pygame.time.Clock()
+wn = pygame.display.set_mode((WIN_X, WIN_Y), pygame.SRCALPHA, vsync=1)
+window = pygame.Surface((WIN_X, WIN_Y), pygame.SRCALPHA)
+RUN = True
+GAME_ENDED = False
+
+game = Game()
+game_surf = pygame.Surface((WIN_X, WIN_Y), pygame.SRCALPHA)
+game_surf_pos = (0,0)
+
+DefaultFont = pygame.font.Font(pygame.font.match_font('timesnewroman'), 44)
+def draw_text(text: str, x: float, y: float, surf: pygame.Surface, font: pygame.font.Font = DefaultFont, color:tuple[int,int,int]=(0,0,0)):
+    surf.blit(font.render(text,True,color), (x,y))
+
+def in_constrain(value:float, minimum:float, maximum:float) -> bool:
+    if (value < minimum): return False
+    if (value > maximum): return False
+    return True
+
+def constrain(value:float, minimum:float, maximum:float) -> float:
+    if (value < minimum): return minimum
+    if (value > maximum): return maximum
+    return value
+
+def uv2xy(surf: pygame.Surface, uv: tuple[float, float]) -> tuple[int, int]:
+    (xsize, ysize) = surf.get_size()
+    (u,v) = uv
+    return (int(constrain(u*xsize, 0, xsize-1)), 
+            int(constrain(v*ysize, 0, ysize-1)))
+
+def draw_game(game: Game, surf: pygame.Surface):
+    BACK = (0, 37, 225, 255)
+    GRAY = (125,125,125, 255)
+    P2 = (255, 126, 0, 255)
+    P1 = (255, 0, 0, 255)
+    surf.fill(BACK)
+    (xsize, ysize) = surf.get_size()
+    rad_uv = (
+        1/(game.columns+1),
+        1/(game.rows+1)
+    )
+    radius = min(uv2xy(surf, rad_uv))/2 - 5
+    for y in range(game.rows):
+        for x in range(game.columns):
+            clr = GRAY
+            ref = game.gamemap[y][x]
+            if (ref == GM.CellEnum.FILLED_P1.value):
+                clr = P1
+            elif (ref == GM.CellEnum.FILLED_P2.value):
+                clr = P2
+            
+            coord_uv = (
+                (x+1)/(game.columns+1),
+                (y+1)/(game.rows+1)
+            )
+            coord_xy = uv2xy(surf, coord_uv)
+            
+            pygame.draw.circle(
+                surf, clr,
+                coord_xy,
+                radius
+            )
+    
+    if game.ended:
+        text = "Game ended"
+        if game.winner == 0:
+            text = "Player 1 won"
+        elif game.winner == 1:
+            text = "Player 2 won"
+        
+        (text_size_x, text_size_y) = DefaultFont.size(text)
+        draw_text(text, (xsize-text_size_x)/2, (ysize-text_size_y)/2, surf)
+
+# -> (clicked_circle, xy_matrix_coords)
+def click_inside_game(game: Game, surf: pygame.Surface, mouse_posrel: tuple[int,int]) -> tuple[bool, tuple[int,int]]:
+    BAD_ANS = (False, (-1,-1))
+    (mx, my) = mouse_posrel
+    if (mx<0 or my<0): return BAD_ANS
+    
+    (xsize, ysize) = surf.get_size()
+    if (mx>=xsize or my>ysize): return BAD_ANS
+    
+    rad_uv = (
+        1/(game.columns+1),
+        1/(game.rows+1)
+    )
+    radius = min(uv2xy(surf, rad_uv))
+    for y in range(game.rows):
+        for x in range(game.columns):
+            coord_uv = (
+                (x+1)/(game.columns+1),
+                (y+1)/(game.rows+1)
+            )
+            coord_xy = uv2xy(surf, coord_uv)
+            (cx, cy) = coord_xy
+            if ( (cx-mx)*(cx-mx) + (cy-my)*(cy-my) <= radius*radius ):
+                return (True, (x,y))
+    
+    return BAD_ANS
+            
+
+while RUN:
+    clock.tick(FPS)
+    window.fill((255,255,255))
+    EVENTS = pygame.event.get()
+    
+    for event in EVENTS:
+        if (event.type == pygame.QUIT):
+            RUN = False # type: ignore
+        if (event.type == pygame.MOUSEBUTTONUP):
+            mouse_pos = pygame.mouse.get_pos()
+            
+            mouse_posrel = (
+                mouse_pos[0] - game_surf_pos[0],
+                mouse_pos[1] - game_surf_pos[1]
+            )
+            
+            if (not in_constrain(mouse_posrel[0], 0, game_surf.get_size()[0]-1) 
+                or  not in_constrain(mouse_posrel[1], 0, game_surf.get_size()[1]-1)):
+                continue
+            
+            if (game.ended):
+                game = Game()
+                GAME_ENDED = False # type: ignore
+                continue
+            
+            (status, coord) = click_inside_game(game, game_surf, mouse_posrel)
+            
+            if (status):
+                if (game.move(coord[0])):
+                    if (game.check_win() > -1):
+                        GAME_ENDED = True # type: ignore
+                    if (not game.are_empty_cells()):
+                        GAME_ENDED = True # type: ignore
+                        
+    keys = pygame.key.get_pressed()
+    
+    if (keys[pygame.K_r]):
+        game = Game()
+        GAME_ENDED = False # type: ignore
+
+    draw_game(game, game_surf)
+    window.blit(game_surf, game_surf_pos)
+    wn.blit(window, (0,0))
+    pygame.display.update()
---
+++ game.py
@@ -0,0 +1,150 @@
+import enum
+
+class CellEnum(enum.Enum):
+    EMPTY = 0
+    FILLED_P1 = 1
+    FILLED_P2 = 2
+
+class Game:
+    def __init__(self, rows:int = 6, columns: int = 7, startturn: int = 0):
+        # matrix[y][x]
+        self.rows = rows
+        self.columns = columns
+        
+        self.lastmove_xy: tuple[int,int] = (0,0)
+
+        self.gamemap: list[ list[int] ] = [ [ CellEnum.EMPTY.value for _ in range(columns)] for _ in range(rows)]
+        
+        # 0 for P1
+        # 1 for P2
+        self.turn: int = startturn
+        
+        self.ended: bool = False
+        
+        # -1 - nobody
+        # 0 - P1
+        # 1 - P2
+        self.winner = -1
+    
+    
+    def are_empty_cells(self) -> bool:
+        for x in range(self.columns):
+            if self.gamemap[0][x] == CellEnum.EMPTY.value:
+                return True
+        self.ended = True
+        self.winner = -1
+        return False
+    
+    def check_win_cell(self, x: int, y: int) -> bool:
+        # print(x, y)
+        ref = self.gamemap[y][x]
+        # print(f"ref = {ref}")
+        inrow = 0
+        for dx in range(-3, 3+1):
+            if x+dx<0: continue
+            if x+dx>=self.columns: break
+            
+            if (self.gamemap[y][x+dx] == ref):
+                inrow+=1
+            else:
+                inrow=0
+            
+            if inrow == 4:
+                # print("on x")
+                return True
+        if inrow == 4:
+            # print("on x o")
+            return True
+        
+        inrow = 0
+        for dy in range(-3, 3+1):
+            if y+dy<0: continue
+            if y+dy>=self.rows: break
+            
+            if (self.gamemap[y+dy][x] == ref):
+                inrow+=1
+            else:
+                inrow=0
+            
+            if inrow == 4:
+                # print("on y")
+                return True
+        if inrow == 4:
+            # print("on y o")
+            return True
+        
+        inrow = 0
+        for di in range(-3, 3+1):
+            if x+di<0 or y+di<0: continue
+            if x+di>=self.columns or y+di>=self.rows: break
+            
+            if (self.gamemap[y+di][x+di] == ref):
+                inrow+=1
+            else:
+                inrow=0
+            
+            if inrow == 4:
+                # print("on x+y")
+                return True
+        if inrow == 4:
+            # print("on x+y o")
+            return True
+        
+        inrow = 0
+        for dj in range(-3, 3+1):
+            if x+dj<0 or y-dj>=self.rows: continue
+            if x+dj>=self.columns or y-dj<0: break
+            
+            if (self.gamemap[y-dj][x+dj] == ref):
+                inrow+=1
+            else:
+                inrow=0
+            
+            if inrow == 4:
+                # print("on x-y")
+                return True
+        if inrow == 4:
+            # print("on x-y o")
+            return True
+        
+        return False
+    
+    # -1 - no win
+    # 0 - P1 won
+    # 1 - P2 won
+    # is based on the last move
+    def check_win(self) -> int:
+        # print(self.lastmove_xy)
+        win = self.check_win_cell(self.lastmove_xy[0], self.lastmove_xy[1])
+        
+        if not win: return -1
+        
+        self.ended = True
+        if (self.gamemap[self.lastmove_xy[1]][self.lastmove_xy[0]] == CellEnum.FILLED_P1.value):
+            self.winner = 0
+            return 0
+        else:
+            self.winner = 1
+            return 1
+            
+    
+    # True -> ok
+    # False -> move is illegal
+    def move(self, column:int) -> bool:
+        fill = CellEnum.FILLED_P1.value
+        if (self.turn == 1):
+            fill = CellEnum.FILLED_P2.value
+        
+        found_empty = False
+        for y in range(self.rows - 1, -1, -1):
+            if (self.gamemap[y][column] == CellEnum.EMPTY.value):
+                self.gamemap[y][column] = fill
+                found_empty = True
+                self.lastmove_xy = (column, y)
+                break
+                
+        if (not found_empty):
+            return False
+        
+        self.turn = (self.turn+1)%2
+        return True
