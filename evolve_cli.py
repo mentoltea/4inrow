fbc9f65e5b69850dc1fcad667e765ac03928b1fa
s7k-windows <benotimprotant@mail.ru>
2025-05-25 19:49:03 +0300

    CLI added

---
+++ evolve_cli.py
@@ -0,0 +1,37 @@
+import evolve
+import argparse
+
+
+def main(input_dir: str|None, output_dir:str|None, batchsize:int, epochs:int, rows:int=6, columns:int=7):
+    if (input_dir==None):
+        b = evolve.random_batch(6, 7, batchsize)
+    else:
+        b = evolve.Batch.load(rows, columns, input_dir)
+    
+    if (output_dir==None): output_dir=f"epoch{epochs}/"
+    
+    b = evolve.evolve(b, epochs)
+    b.save(output_dir)
+    
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description="Args")
+    
+    
+    parser.add_argument('batchsize', type=int, help='Size of one batch (epoch)')
+    parser.add_argument('epochs', type=int, help='Number of epochs')
+    
+    parser.add_argument('--indir', type=str, help='Input batch folder', default=None)
+    parser.add_argument('--outdir', type=str, help='Output batch folder', default=None)
+    parser.add_argument('--rows', type=int, help='Rows', default=6)
+    parser.add_argument('--columns', type=int, help='Columns', default=7)
+    Args = parser.parse_args()
+    
+    main(
+        input_dir=Args.indir,
+        output_dir=Args.outdir,
+        batchsize=Args.batchsize,
+        epochs=Args.epochs,
+        rows=Args.rows,
+        columns=Args.columns
+    )
